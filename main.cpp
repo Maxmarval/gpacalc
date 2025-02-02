12747154848ae890ff1e00d050720cf214537331
BuildTools <unconfigured@null.spigotmc.org>
2025-02-02 11:02:18 -0600

    final working code

--- main.cpp
+++ main.cpp
@@ -1,41 +1,59 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    int fullYearClasses = 0;
-    int semesterClasses = 0;
-    int x = 0;
-    int fullYearGrade = 0;
-    int semesterGrade = 0;
+int main()
+{
+    bool run = true;
+    char choice;
 
-    cout << "Enter number of full year classes: ";
-    cin >> x;
-    int* arr = new int[x];
-    for (int i = 0; i < x; i++) {
-        cout << "Enter grade for class " << i + 1 << ": ";
-        cin >> arr[i];
-    }
-    for (int i = 0; i < x; i++) {
-        fullYearGrade += arr[i];
-        fullYearGrade /= x;
-    }
+    while (run)
+    {
+        int fullYearClasses = 0;
+        int semesterClasses = 0;
+        int numberofC = 0;
+        int fullYearGrade = 0;
+        int semesterGrade = 0;
 
-    cout << "Enter number of semester classes: ";
-    cin >> x;
-    int* arr2 = new int[x];
-    for (int i = 0; i < x; i++) {
-        cout << "Enter grade for class " << i + 1 << ": ";
-        cin >> arr2[i];
-    }
-    for (int i = 0; i < x; i++) {
-        semesterGrade += arr2[i];
-        semesterGrade /= x ;
-    }
+        cout << "Enter number of full year classes: ";
+        cin >> numberofC;
+        int *arr = new int[numberofC];
+        for (int i = 0; i < numberofC; i++)
+        {
+            cout << "Enter grade for class " << i + 1 << ": ";
+            cin >> arr[i];
+        }
+        for (int i = 0; i < numberofC; i++)
+        {
+            fullYearGrade += arr[i];
+        }
+        fullYearGrade /= numberofC;
 
-    delete[] arr;
-    delete[] arr2;
+        cout << "Enter number of semester classes: ";
+        cin >> numberofC;
+        int *arr2 = new int[numberofC];
+        for (int i = 0; i < numberofC; i++)
+        {
+            cout << "Enter grade for class " << i + 1 << ": ";
+            cin >> arr2[i];
+        }
+        for (int i = 0; i < numberofC; i++)
+        {
+            semesterGrade += arr2[i];
+        }
+        semesterGrade /= numberofC;
 
-   cout << "Your GPA:" << (fullYearGrade * .66 + semesterGrade * .33);
-   cin.get();
-     return 0;
+        delete[] arr;
+        delete[] arr2;
+        cout << fullYearGrade << " " << semesterGrade << endl;
+        cout << "Your GPA:" << (fullYearGrade * 2 + semesterGrade) / 3 << endl;
+        cout << "Run again? (y/n): ";
+        cin >> choice;
+        if (choice == 'n' || choice == 'N')
+        {
+            run = false;
+
+            cin.get();
+        }
+    }
+    return 0;
 }
